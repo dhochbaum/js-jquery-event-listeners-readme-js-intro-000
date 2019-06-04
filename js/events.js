cf969e009137c8d86be30df2f88a80b22bf12a7b
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-06-04 23:27:53 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,10 +19,11 @@ function frameIt() {
   $('img').on('load', function(){
     //console.log(image.attributes.className);
     //image[0].className = "tasty";
-    //$( "img" ).addClass = "tasty";
+    $( "img" ).addClass = "tasty";
     //image.addClass = "tasty";
     //$('img:className').val() = "tasty";
     //console.log($('img'));
     console.log('it is firing at least')
+    
   });
 }
