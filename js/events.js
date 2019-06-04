2880a6b40308e5b55e68c1f007a480b2a18168cb
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-06-04 23:21:00 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function frameIt() {
   $('img').on('click', function(image){
     //console.log(image.attributes.className);
     //image[0].className = "tasty";
-    $('img').style.className = "tasty";
+    $('img').addClass = "tasty";
     //$('img:className').val() = "tasty";
     console.log($('img'));
   });
